d16cf9e5c1c437061e9401021dfd7541d4a7cf61
Hameed Khan <hameed404404@gmail.com>
2026-08-29 08:49:23 +0500

    Added event key and signing key for inngest realtime

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  env: {
+    INNGEST_BASE_URL:
+      process.env.INNGEST_BASE_URL ||
+      (process.env.NODE_ENV === "production"
+        ? "https://api.inngest.com"
+        : "http://localhost:8288"),
+    INNGEST_DEV: process.env.NODE_ENV === "production" ? "false" : "true",
+  },
 };
 
 export default withSentryConfig(nextConfig, {
